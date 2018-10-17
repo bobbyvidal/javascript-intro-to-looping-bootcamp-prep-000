50d4c9a6294ae49b3243d6a2a7bcab6551ad2923
Robert Vidal <bobbyvidal@gmail.com>
2018-10-17 03:48:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,12 @@
 function forLoop(Array){
   for ( var i = 0; i < 10; i++)  {
-    if (i == 1)console.log("I am ${i} strange loops.")
-}}
+    if (i == 1); {
+      console.log("I am ${i} strange loops.")
+      
+    }
+    else ;
+    
+    
+    
+    
+  }}
